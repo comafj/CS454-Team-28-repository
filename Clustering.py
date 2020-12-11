cfceb94cb79eceee1854e06a059f65504667ea6a
deokhwa <ejrghk976@gmail.com>
2020-12-12 03:23:11 +0900

    add captions

--- Clustering.py
+++ Clustering.py
@@ -1,17 +1,20 @@
 import random
 
-
+#Clustering was implemented as one of the methods for application in secondary search.
+#The number of groups to be clustered and the entire element is entered.
 def Clustering(list, k):
     if len(list)>=k:
         Cluster= [set('') for i in range (k)]
         total_idx=0
         while True:
             temp_cluster= [[] for i in range (k)]
-            centriod = random.sample(list, k)
+            #Select as many random points as k and set them as centroid.
+            centroid = random.sample(list, k)
+            #Divide a set of points based on their distance from the centroid.
             for i in range (k):
-                temp_cluster[i].append(centriod[i])
+                temp_cluster[i].append(centroid[i])
             for i in range (len(list)):
-                if list[i] in centriod:
+                if list[i] in centroid:
                     continue
                 idx= 0
                 dist= Find_dist(temp_cluster[0][0], list[i])
@@ -31,6 +34,7 @@ def Clustering(list, k):
 
             total_idx-= len(temp_cluster_set)
 
+            #After saving the results, return the values if they are the same as the results of the previous trial.
             Cluster = temp_cluster_set
             if total_idx < (50-len(temp_cluster_set)):
                 total_idx+= 1
@@ -38,17 +42,12 @@ def Clustering(list, k):
                     print(Cluster)
                     break
             else:
-
                 total_idx=0
 
 
-
+#It is a function that finds distance.
+#At this time, the distance does not use Euclidean space.
+#Because the RGB value is an integer only, the sum of the differences in each value is set as the distance.
 def Find_dist(elem1, elem2):
     return abs(elem1[0]-elem2[0])+abs(elem1[1]-elem2[1])+abs(elem1[2]-elem2[2])
-
-
-
-#cnd_elem = [(1,1,1),(1,2,1),(5,1,1),(12,1,41),(1,12,1),(1,15,1),(1,1,2),(8,2,1),(7,2,4)]
-#cnd_elem = [(1,1,1),(1,2,1),(5,1,1),(12,1,41)]
-#Clustering(cnd_elem, 2)
 
--- change_color.py
+++ change_color.py
@@ -1,64 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.color import Color
-from webdriver_manager.chrome import ChromeDriverManager
-
-def color_element_from_url(url):
-    # browser = webdriver.Chrome()
-    
-
-    candi_elems = browser.find_elements_by_xpath("//*[contains (@ style, 'color')]")
-    result = []
-
-    index = 1
-    for ce in candi_elems:
-        single_r = [] # single_r 형식 : [index, text element, text color, background element, background color]
-        single_r.append(index)
-        
-        p_of_ce = ce.find_element_by_xpath("..")
-        if ce.text == "":
-            continue
-        style_of_ce = ce.get_attribute("style")
-        p_back = p_of_ce.value_of_css_property("background-color") 
-        for soc in style_of_ce.split(";"):
-            if "color" in soc:
-                cstr = soc.split(":")[1].strip()
-                ccol = Color.from_string(cstr) # 글자 색깔을 Color class로 표현
-                single_r.append(ce)
-                single_r.append(ccol)
-                break
-        p_back = Color.from_string(p_back) # 배경 색깔을 Color class로 표현
-        single_r.append(p_of_ce)
-        single_r.append(p_back)
-        index+=1
-        result.append(single_r)
-    #browser.quit()
-    return result
-
+#The Web Element Type object is entered.
 def change_color(browser, elem, color, cf):
-
+    #Change the browser after deciding whether to change the background or color of the writing.
     if cf == 'b':
         browser.execute_script("arguments[0].setAttribute(arguments[1], arguments[2]);", elem, "style", elem.get_attribute("style")+"background-color:"+color)
     elif cf == 't':
         browser.execute_script("arguments[0].setAttribute(arguments[1], arguments[2]);", elem, "style", elem.get_attribute("style")+"color:"+color)
     
-    
-
-
-
-if __name__ == "__main__":
-    test_url = "https://sattamatkamarket.in/"
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser.get(test_url)
-    test_arr = color_element_from_url(test_url)
-    '''
-    for i in range (len(test_arr)/2):
-    	if len(test_arr[i])>2:
-    		change_color(test_arr[i][3], "blue")
-    	else:
-    		continue
-    '''
-    change_color(browser, test_arr[8][1], "pink", 'b')
-    change_color(browser, test_arr[9][1], "#0000FF", 'b')
-    
-
